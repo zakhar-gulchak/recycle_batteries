c89cc304ef71ded4448eff3c94ec92fbe8f63609
Zakhar Gulchak <gulchak.zahar@gmail.com>
2014-08-29 15:47:20 +0300

    Add sql with db

---
+++ db/db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE `recycle_batteries`;
+USE `recycle_batteries`;
+CREATE TABLE `battery` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) NOT NULL,
+  `count` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
